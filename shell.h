e8e14c368a5b9067a65b823990919b1da609a967
Ajimmy-james <jamesobuyas@gmail.com>
2022-05-19 13:44:35 -0700

    Simple Shell

--- shell.h
+++ shell.h
@@ -1,116 +1,137 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
+
+#include <fcntl.h>
+#include <signal.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+/* Global environemnt */
+extern char **environ;
+/* Global program name */
+char *name;
+/* Global history counter */
+int hist;
 
 /**
- * struct Hshpack - struct containing important shell info
- * @hshname: name of the shell (argv[0])
- * @buffer: full buffer written in prompt
- * @cmd: command written after prompt
- * @options: options of the command
- * @path: path of the cmd if found, else cmd itself
- * @errnum: error count
- * @exitnum: number of exit that the shell will have
- * @relation: true or false for logical operators
- * @run_able: check for running or not a command
- * @next: singly list link
- * @envCpy: copy of environment
- * @unsetnull: check for setting environment to NULL
- *
- * Description: Struct containing important shell info
- *
+ * struct list_s - A new struct type defining a linked list.
+ * @dir: A directory path.
+ * @next: A pointer to another struct list_s.
  */
-typedef struct Hshpack
+typedef struct list_s
 {
-	char *hshname;        /* Name of shell argv[0] */
-	char *buffer;         /* complete line written in prompt */
-	char *cmd;            /* current command */
-	char **options;       /* options of current command */
-	char *path;           /* absolute path of command */
-	int *errnum;          /* error number for message */
-	int *exitnum;         /* exit number */
-	int *relation;        /* 0 Undef, 1 ||, 2 && */
-	int *run_able;        /* 0 False 1 True (Is able to run)*/
-	struct Hshpack *next; /*adress of next cmd to run */
-	char ***envCpy;       /* current environment */
-	int *unsetnull;       /*check for setting environment to NULL */
-
-} hshpack;
+	char *dir;
+	struct list_s *next;
+} list_t;
 
 /**
- * struct b_ins - struct for built ins
- * @cmd: built in
- * @f: function of built in
- *
- * Description: Struct for calling built in functions
- *
+ * struct builtin_s - A new struct type defining builtin commands.
+ * @name: The name of the builtin command.
+ * @f: A function pointer to the builtin command's function.
  */
-typedef struct b_ins
+typedef struct builtin_s
 {
-	char *cmd;
-	ssize_t (*f)(hshpack *shpack);
-} b_ins;
+	char *name;
+	int (*f)(char **argv, char **front);
+} builtin_t;
 
 /**
- * struct Helps - struct for built ins
- * @built: built in command
- * @h: help function of built in
- *
- * Description: Struct for calling built in functions
- *
+ * struct alias_s - A new struct defining aliases.
+ * @name: The name of the alias.
+ * @value: The value of the alias.
+ * @next: A pointer to another struct alias_s.
  */
-typedef struct Helps
+typedef struct alias_s
 {
-	char *built;
-	void (*h)(void);
-} helps;
+	char *name;
+	char *value;
+	struct alias_s *next;
+} alias_t;
 
+/* Global aliases linked list */
+alias_t *aliases;
 
+/* Main Helpers */
+ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char **_strtok(char *line, char *delim);
+char *get_location(char *command);
+list_t *get_path_dir(char *path);
+int execute(char **args, char **front);
+void free_list(list_t *head);
+char *_itoa(int num);
 
+/* Input Helpers */
+void handle_line(char **line, ssize_t read);
+void variable_replacement(char **args, int *exe_ret);
+char *get_args(char *line, int *exe_ret);
+int call_args(char **args, char **front, int *exe_ret);
+int run_args(char **args, char **front, int *exe_ret);
+int handle_args(int *exe_ret);
+int check_args(char **args);
+void free_args(char **args, char **front);
+char **replace_aliases(char **args);
 
-char *_getenv(const char *name, char **env);
-char *_path(char *cmd, char **env, hshpack *shpack);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-char *_strtok(char *str, const char *delim);
-char **getParameters(char *buffer, hshpack *shpack);
-int executeCmd(char *program, char *command[], char **env, hshpack *shpack);
-void signal_handler(int x);
-void signal_handler2(int x);
-int _getline(char **buffer, size_t *bufsize, int fd);
-void free_doubpoint(char **p);
-int _strlendp(char **s);
-char **checkInput(int ac, char **av, size_t *bufsize,
-		  char **buffer, hshpack *shpack);
-hshpack *set_struct(char *argv0, int *errn, int *exnum, int *relation,
-		    int *run_able, char ***env, int *unsetnull);
-int _error(int errn, hshpack *shpack, int exnum);
-void addCmd(hshpack *shpack, char *buffer, char *command, char **parameters);
-void addPathToCmd(hshpack *shpack, char *pathCmd);
-ssize_t built_ints(hshpack *shpack);
-ssize_t _exit_cmd(hshpack *shpack);
+/* String functions */
+int _strlen(const char *s);
+char *_strcat(char *dest, const char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+char *_strcpy(char *dest, const char *src);
+char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
 int _strcmp(char *s1, char *s2);
-long _atoi(char *s);
-long _pow(long base, long pot);
-char **_copydoublep(char **p, int old_size, int new_size);
-int _strlendp(char **s);
-char **_setenv(char **env, char *variable, char *value, hshpack *shpack);
-char **_unsetenv(char **env, char *variable, hshpack *shpack);
-int _isdigit(int c);
-int _isnumber(char *s);
-ssize_t _cd_cmd(hshpack *shpack);
-char *deleteComment(char *str);
+int _strncmp(const char *s1, const char *s2, size_t n);
 
+/* Builtins */
+int (*get_builtin(char *command))(char **args, char **front);
+int shellby_exit(char **args, char **front);
+int shellby_env(char **args, char __attribute__((__unused__)) **front);
+int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int shellby_cd(char **args, char __attribute__((__unused__)) **front);
+int shellby_alias(char **args, char __attribute__((__unused__)) **front);
+int shellby_help(char **args, char __attribute__((__unused__)) **front);
 
-ssize_t _help_cmd(hshpack *shpack);
-void _puts(char *s);
-void help_unsetenv(void);
-void help_cd(void);
-void help_help(void);
+/* Builtin Helpers */
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
+
+/* Error Handling */
+int create_error(char **args, int err);
+char *error_env(char **args);
+char *error_1(char **args);
+char *error_2_exit(char **args);
+char *error_2_cd(char **args);
+char *error_2_syntax(char **args);
+char *error_126(char **args);
+char *error_127(char **args);
+
+/* Linkedlist Helpers */
+alias_t *add_alias_end(alias_t **head, char *name, char *value);
+void free_alias_list(alias_t *head);
+list_t *add_node_end(list_t **head, char *dir);
+void free_list(list_t *head);
+
+void help_all(void);
 void help_alias(void);
-void printsHelp(void);
+void help_cd(void);
+void help_exit(void);
+void help_help(void);
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+int proc_file_commands(char *file_path, int *exe_ret);
 
 #endif
